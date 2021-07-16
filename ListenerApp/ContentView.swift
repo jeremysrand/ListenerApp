c9e72aea6a6a6712b0925eab02aa051053006f81
Jeremy Rand <jeremy@rand-family.com>
2021-07-16 12:46:54 -0400

    Initial Commit

---
+++ ListenerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListenerApp
+//
+//  Created by Jeremy Rand on 2021-07-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListenerApp/ListenerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListenerAppApp.swift
+//  ListenerApp
+//
+//  Created by Jeremy Rand on 2021-07-16.
+//
+
+import SwiftUI
+
+@main
+struct ListenerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
